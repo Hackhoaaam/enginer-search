efaff097480a7b43f0e52f0dc0f8554e3ccef942
Hackhoaaam <38091021+Hackhoaaam@users.noreply.github.com>
2018-08-09 01:47:26 +0700

    Update Operator.cpp

--- Operator.cpp
+++ Operator.cpp
@@ -1,5 +1,6 @@
 #include"Operator.h"
 #include"Header.h"
+#include<string>
 #include"Highlight.h"
 
 bool is_txt(string text) {
@@ -72,4 +73,59 @@ bool is_OR(string text) {
 		if (And != -1 && text[i] != ' ')return true;
 	}
 	return false;
+}
+
+bool is_included(string text) {
+	bool fw = false;
+	int incl = text.find("+");
+	if (incl <= 0 || incl > text.size() || text[incl - 1] != ' ') return false;
+	for (int i = 0; i < incl - 1; ++i)
+		if (text[i] != ' ') fw = true;
+	for (int i = incl + 1; i < text.size() && fw == true; i++)
+		if (text[i] != ' ') return true;
+	return false;
+}
+
+bool isWildCard(string query)
+{
+	if (isExact(query))
+	{
+		if (query.find('*') != -1) return true;
+		else return false;
+	}
+	return false;
+}
+
+bool isExact(string query)
+{
+	int start = query.find('"');
+	int end = query.find('"', start + 1);
+	if (start != -1 && end != -1)
+		return true;
+	else return false;
+}
+//write some function
+bool isExist(string keyword, keyblock &tmp)
+{
+	string path = "D:\\Gruop02\\CS163-Project-Data\\";
+	ifstream fin; string fullpath = path + tmp.title + ".txt";
+	fin.open(fullpath);
+	string sample;
+	int pos = 0;
+	bool check = false;
+	while (!fin.eof())
+	{
+		getline(fin, sample);
+		lowercase(sample);
+		if (sample.empty())
+			++pos;
+		if (sample.find(keyword) != -1)
+		{
+			tmp.frequency++;
+			tmp.para[pos]++;
+			check = true;
+		}
+	}
+	fin.close();
+	return check;
 }
