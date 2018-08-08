2eabbe14c4781530edb563e7b571e19ad2806ca9
Hackhoaaam <38091021+Hackhoaaam@users.noreply.github.com>
2018-08-09 01:47:55 +0700

    Update Operator.h

--- Operator.h
+++ Operator.h
@@ -9,5 +9,11 @@ bool is_hashtag(string text);
 bool is_title(string text);
 bool is_exclude(string text);
 bool is_OR(string text);
+bool is_included(string text);
+bool is_title(string text);
+bool isWildCard(string query);
+bool is_included(string text);
+bool isExact(string query);
+
 
 #endif
