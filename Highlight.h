cf3e22b8475d6165b5a60e0178cfba0712694cc0
Hackhoaaam <38091021+Hackhoaaam@users.noreply.github.com>
2018-08-09 01:46:47 +0700

    Update Highlight.h

--- Highlight.h
+++ Highlight.h
@@ -7,13 +7,12 @@
 #include"Header.h"
 #include <Windows.h>
 
-					
+
 void color_print(string &text);				//print the word in color
 void normalize(string &text);				//lower case the word + remove punctuation
-void to_lower(string &text);			
-void remove_puctuation(string &word);		
+void to_lower(string &text);
+void remove_puctuation(string &word);
 bool is_qry(vector<string> queries, int n, string word);							//check if a word is == query							
 void highLightQueries(vector<string> queries, int n, string text);					//highlight queries in the text
 void hightlight_para(string file_name, int para, vector<string> queries);			//highlight the all the queries in a paragraph (para th) from file_name.txt
 #endif // !HIGHLIGHT
-
